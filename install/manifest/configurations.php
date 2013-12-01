26c89bd337805a43e6f6c030ebdb13cd3a4f3b74
souzabrizolara <souzabrizolara@gmail.com>
2013-12-01 14:03:19 -0300

    Modificado install/manifest/configurations para obter configurações de HubConf.

--- install/manifest/configurations.php
+++ install/manifest/configurations.php
@@ -12,30 +12,17 @@ function fixSeparator($path) {
  * @return algo
  */
 function manifestDetails() {
-    // Definindo o prefixo de instalação baseado no root do servidor
-    if(php_sapi_name() == 'cli'){
-        $configs = HubConf::getConfigurations(); 
-        $prefix = realpath($configs['project_root']. DIRECTORY_SEPARATOR . '..' ).DIRECTORY_SEPARATOR;
-        
-        
-//        echo $prefix;
-        
-        
-    }else{
-        $prefix = filter_input(INPUT_SERVER, 'DOCUMENT_ROOT') . DIRECTORY_SEPARATOR;
-    }
 
     // Carregando as diretrizes
     $d = DIRECTORY_SEPARATOR;
-    $hubFile = dirname(__FILE__) . $d . '..' . $d . '..' . $d . 'hub.ini';
     $configFile = dirname(__FILE__) . $d . 'config' . $d . 'directives.json';
-
-    if (is_file($configFile) && is_file($hubFile)) {
+    $hubConfigs = HubConf::getConfigurations();
+    
+    if (is_file($configFile) && !is_null($hubConfigs)) {
         $directives = json_decode(file_get_contents($configFile), true);
-        $iniFile = parse_ini_file($hubFile);
 
-        $projectRoot = $prefix . fixSeparator($iniFile['projectRoot']);
-        $output = $projectRoot . $d . ($iniFile['manifestPath']);
+        $projectRoot = $hubConfigs['project_root'];
+        $output = $projectRoot . $d . ($hubConfigs['manifest_path']);
 
         $codePacks = $directives['contents'];
         return array('root' => $projectRoot, 'output' => $output, 'codePacks' => $codePacks);
