fa200db8e8cb13810600cc35d0a8e4c3cfc296b4
souzabrizolara <souzabrizolara@gmail.com>
2013-12-03 16:12:18 -0300

    testes para HomeIntentManager

--- src/intents/home/HomeIntent.class.php
+++ src/intents/home/HomeIntent.class.php
@@ -6,6 +6,9 @@ class HomeIntent{
 	private $structure;
 	private $subIntents;
 	
+        /**
+         * @return househub\intents\IntentStructure Description
+         */
 	public function getStructure() { return $this->structure; } 
 	public function getSubIntents() { return $this->subIntents; }
 	 
--- src/intents/home/HomeIntentManager.class.php
+++ src/intents/home/HomeIntentManager.class.php
@@ -21,82 +21,92 @@ use PDO;
 
 class HomeIntentManager{
 	
-	public function loadIntent($identifier, PDO $driver){
-		$intent = new HomeIntent();
-		
-		$intent->setStructure($this->loadStructure($identifier, $driver));
-		$intent->setSubIntents($this->loadSubIntents($identifier, $driver));
-		
-		return $intent;
-	}
-	
-	public function loadStructure($identifier, PDO $driver){
-		if(!is_numeric($identifier)){
-			return null;
-		}
-		
-		$dao = new IntentStructureDAO($driver);
-		$structure = $dao->load($identifier);
-		
-		return $structure;
-	}
-	
-	public function loadSubIntents($identifier, PDO $driver){
-		$intents = array();
-		
-		$select = new SelectQuery();
-		$select->addColumn(IntentStructureTable::COLUMN_ID);
-		$select->setEntity(IntentStructureTable::TABLE_NAME);
-		
-		$criteria = new SqlCriteria();
-		$criteria->add(new SqlFilter(IntentStructureTable::COLUMN_PARENT_ID, '=', $identifier));
-		$select->setCriteria($criteria);
-		
-		$statement = $driver->query($select->getInstruction());
-		while($rs = $statement->fetch(PDO::FETCH_ASSOC)){
-			$intents[] = $this->loadIntent(intval($rs[IntentStructureTable::COLUMN_ID]), $driver);
-		}
-		
-		return $intents;
-	}
-	
-	public function saveIntent(HomeIntent $homeIntent, PDO $driver){
-		$intent = $homeIntent;
-		$structure = $intent->getStructure();
-		
-		$intentDAO = new IntentStructureDAO($driver);
-		$intentId = $intentDAO->insert($structure);
-		$structure->setId($intentId);
-		
-		$subIntents = $intent->getSubIntents();
-		foreach($subIntents as $key=>$localIntent){
-			$localStructure = $localIntent->getStructure();
-			$localStructure->setParentId($intentId);
-			$localIntent->setStructure($localStructure);
-			
-			$insertStructure = $this->saveIntent($localIntent, $driver);
-			$subIntents[$key] = $insertStructure;
-		}
-		
-		$intent->setStructure($structure);
-		$intent->setSubIntents($subIntents);
-		
-		return $intent;
-	}
-	
-	public function deleteIntent(HomeIntent $intent, PDO $driver){
-		$delete = new DeleteQuery();
-		$intentId = $intent->getStructure()->getId();
-		
-		$delete->setEntity(IntentStructureTable::TABLE_NAME);
-		
-		$criteria = new SqlCriteria();
-		$criteria->add(new SqlFilter(IntentStructureTable::COLUMN_ID, '=', $intentId), SqlExpression::OR_OPERATOR);
-		$criteria->add(new SqlFilter(IntentStructureTable::COLUMN_PARENT_ID, '=', $intentId), SqlExpression::OR_OPERATOR);
-		$delete->setCriteria($criteria);
-		
-		return $driver->exec($delete->getInstruction());
-	}
+    /**
+     * @return househub\intents\home\HomeIntent
+     */
+    public function loadIntent($identifier, PDO $driver) {
+        $structure = $this->loadStructure($identifier, $driver);
+        
+        if(!is_null($structure)){
+            $intent = new HomeIntent();
+            $intent->setStructure($structure);
+            $intent->setSubIntents($this->loadSubIntents($identifier, $driver));
+
+            return $intent;
+        }
+        return null;
+    }
+
+    public function loadStructure($identifier, PDO $driver) {
+        if (!is_numeric($identifier)) {
+            return null;
+        }
+
+        $dao = new IntentStructureDAO($driver);
+        $structure = $dao->load($identifier);
+
+        return $structure;
+    }
+
+    public function loadSubIntents($identifier, PDO $driver) {
+        $intents = array();
+
+        $select = new SelectQuery();
+        $select->addColumn(IntentStructureTable::COLUMN_ID);
+        $select->setEntity(IntentStructureTable::TABLE_NAME);
+
+        $criteria = new SqlCriteria();
+        $criteria->add(new SqlFilter(IntentStructureTable::COLUMN_PARENT_ID, '=', $identifier));
+        $select->setCriteria($criteria);
+
+        $statement = $driver->query($select->getInstruction());
+        while ($rs = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $intents[] = $this->loadIntent(intval($rs[IntentStructureTable::COLUMN_ID]), $driver);
+        }
+
+        return $intents;
+    }
+
+    public function saveIntent(HomeIntent $homeIntent, PDO $driver) {
+        $intent = $homeIntent;
+        $structure = $intent->getStructure();
+
+        $intentDAO = new IntentStructureDAO($driver);
+        $intentId = $intentDAO->insert($structure);
+        $structure->setId($intentId);
+
+        $subIntents = $intent->getSubIntents();
+        if(!is_null($subIntents)){
+            foreach ($subIntents as $key => $localIntent) {
+                $localStructure = $localIntent->getStructure();
+                $localStructure->setParentId($intentId);
+                $localIntent->setStructure($localStructure);
+
+                $insertStructure = $this->saveIntent($localIntent, $driver);
+                $subIntents[$key] = $insertStructure;
+            }
+        }
+
+        $intent->setStructure($structure);
+        $intent->setSubIntents($subIntents);
+
+        return $intent;
+    }
+
+    public function deleteIntent(HomeIntent $intent, PDO $driver) {
+        $delete = new DeleteQuery();
+        $intentId = $intent->getStructure()->getId();
+
+        $delete->setEntity(IntentStructureTable::TABLE_NAME);
+
+        $criteria = new SqlCriteria();
+        $criteria->add(new SqlFilter(IntentStructureTable::COLUMN_ID, '=', $intentId), SqlExpression::OR_OPERATOR);
+        $criteria->add(new SqlFilter(IntentStructureTable::COLUMN_PARENT_ID, '=', $intentId), SqlExpression::OR_OPERATOR);
+        $delete->setCriteria($criteria);
+
+        return $driver->exec($delete->getInstruction());
+    }
+
 }
 
 ?>
