91439c3b36724be87596731bd9a1a3729b3c6ab5
souzabrizolara <souzabrizolara@gmail.com>
2013-12-04 15:18:48 -0300

    testando access

--- src/access/strategies/user/AddUserAccessStrategy.class.php
+++ src/access/strategies/user/AddUserAccessStrategy.class.php
@@ -1,133 +1,127 @@
 <?php
+
 // Complete
+
 namespace househub\access\strategies\user;
 
 use househub\users\UserStructure;
-
 use lightninghowl\utils\sql\SqlFilter;
-
 use lightninghowl\utils\sql\SqlCriteria;
-
 use lightninghowl\utils\sql\SelectQuery;
-
 use househub\access\strategies\AbstractAccessStrategy;
-
 use househub\users\tables\UserStructureTable;
-
 use househub\users\session\SessionManager;
 use househub\users\dao\UserStructureDAO;
-
 use househub\access\DatabaseConnector;
 
-class AddUserAccessStrategy extends AbstractAccessStrategy{
-    const NAME_ARG   = "name";
-    const NICK_ARG   = "nickname";
+class AddUserAccessStrategy extends AbstractAccessStrategy {
+
+    const NAME_ARG = "name";
+    const NICK_ARG = "nickname";
     const GENDER_ARG = "gender";
-    const LOGIN_ARG  = "login";
-    const PASS_ARG   = "password";
-    
+    const LOGIN_ARG = "login";
+    const PASS_ARG = "password";
+    const USER_ARG = "user_id";
+
     private $dbDriver;
-    
+
     public function __construct($driver = null) {
-        $this->dbDriver = (is_null($driver)? DatabaseConnector::getDriver() : $driver);
+        $this->dbDriver = (is_null($driver) ? DatabaseConnector::getDriver() : $driver);
     }
 
-
-    public function requestAccess($parameters){
+    public function requestAccess($parameters) {
         $answer = $this->initializeAnswer();
-        
-        if($this->checkSession()){
-            if($this->checkParameters($parameters)){
+
+        if ($this->checkSession()) {
+            if ($this->checkParameters($parameters)) {
                 $this->createUser($parameters, $answer);
-            }
-            else{
+            } else {
                 $answer->setMessage('@error');
             }
-        }else{
+        } else {
             $answer->setMessage('@forbidden');
         }
 
         return $answer;
     }
-    
-    protected function checkSession(){
+
+    protected function checkSession() {
         $sessManager = SessionManager::getInstance();
-        $userId = $sessManager->getSessionVariable('user_id');
+        $userId = $sessManager->getSessionVariable(self::USER_ARG);
         return (!is_null($userId) && is_numeric($userId) && $userId >= 0);
     }
-    
-    protected function checkParameters(&$parameters){        
-        if($this->checkName($parameters)
-                && $this->checkNickname($parameters)
-                && $this->checkGender($parameters)
-                && $this->checkLogin($parameters)
-                && $this->checkPassword($parameters))
-        {
-            
-            $parameters[self::NAME_ARG]  = urldecode($parameters[self::NAME_ARG]);
-            $parameters[self::NICK_ARG]  = urldecode($parameters[self::NICK_ARG]);
-            $parameters[self::GENDER_ARG]= urldecode($parameters[self::GENDER_ARG]);
+
+    protected function checkParameters(&$parameters) {
+        if ($this->checkName($parameters) && $this->checkNickname($parameters) && $this->checkGender($parameters) && $this->checkLogin($parameters) && $this->checkPassword($parameters)) {
+
+            $parameters[self::NAME_ARG] = urldecode($parameters[self::NAME_ARG]);
+            $parameters[self::NICK_ARG] = urldecode($parameters[self::NICK_ARG]);
+            $parameters[self::GENDER_ARG] = urldecode($parameters[self::GENDER_ARG]);
             $parameters[self::LOGIN_ARG] = strtolower(urldecode($parameters[self::LOGIN_ARG]));
-            $parameters[self::PASS_ARG]  = urldecode($parameters[self::PASS_ARG]);
-            
+            $parameters[self::PASS_ARG] = urldecode($parameters[self::PASS_ARG]);
+
             return true;
         }
         return false;
     }
-    public function checkName($parameters){
+
+    public function checkName($parameters) {
         return isset($parameters[self::NAME_ARG]) && (strlen($parameters[self::NAME_ARG]) <= 100);
     }
-    public function checkNickname($parameters){
+
+    public function checkNickname($parameters) {
         return isset($parameters[self::NICK_ARG]) && (strlen($parameters[self::NICK_ARG]) <= 20);
     }
-    public function checkGender($parameters){
+
+    public function checkGender($parameters) {
         return isset($parameters[self::GENDER_ARG]);
     }
-    public function checkLogin($parameters){
+
+    public function checkLogin($parameters) {
         return isset($parameters[self::LOGIN_ARG]) && (strlen($parameters[self::LOGIN_ARG]) <= 25);
     }
-    public function checkPassword($parameters){
+
+    public function checkPassword($parameters) {
         return isset($parameters[self::PASS_ARG]) && (strlen($parameters[self::PASS_ARG]) <= 12);
     }
-    
-    protected function createUser($parameters,&$answer){
-        if(!$this->existUser($parameters[self::LOGIN_ARG])){
+
+    protected function createUser($parameters, &$answer) {
+        if (!$this->existUser($parameters[self::LOGIN_ARG])) {
             $newUser = $this->makeUser($parameters);
-            
+
             $userDAO = new UserStructureDAO($this->dbDriver);
             $userDAO->insert($newUser);
 
             $answer->setStatus(1);
             $answer->setMessage('@success');
-        }
-        else{
+        } else {
             $answer->setMessage('@login_already_taken');
         }
     }
-    
-    protected function makeUser($parameters){
+
+    protected function makeUser($parameters) {
         $newUser = new UserStructure();
         $newUser->setName($parameters[self::NAME_ARG]);
         $newUser->setNickname($parameters[self::NICK_ARG]);
         $newUser->setGender($parameters[self::GENDER_ARG]);
         $newUser->setUsername($parameters[self::LOGIN_ARG]);
-        $newUser->setPassword($parameters[self::PASS_ARG]);        
+        $newUser->setPassword($parameters[self::PASS_ARG]);
         return $newUser;
     }
-    
-    protected function existUser($login){
+
+    protected function existUser($login) {
         $driver = DatabaseConnector::getDriver();
-            
+
         $select = new SelectQuery();
         $select->addColumn(UserStructureTable::COLUMN_ID);
         $select->setEntity(UserStructureTable::TABLE_NAME);
 
         $criteria = new SqlCriteria();
-        $criteria->add(new SqlFilter('LCASE('.UserStructureTable::COLUMN_USERNAME.')', 'like', $login));
+        $criteria->add(new SqlFilter('LCASE(' . UserStructureTable::COLUMN_USERNAME . ')', 'like', $login));
         $select->setCriteria($criteria);
 
         $statement = $driver->query($select->getInstruction());
-        
+
         return ($statement->rowCount() > 0);
     }
 
--- src/access/strategies/user/GrantViewAccessStrategy.class.php
+++ src/access/strategies/user/GrantViewAccessStrategy.class.php
@@ -1,71 +1,64 @@
 <?php
+
 // Complete
+
 namespace Core\Access\Strategies\User;
 
+use househub\access\DatabaseConnector;
+use househub\access\strategies\AbstractAccessStrategy;
+use househub\users\session\SessionManager;
 use LightningHowl\Utils\Sql\InsertQuery;
 
-use Core\User\SessionManager;
+class GrantViewAccessStrategy extends AbstractAccessStrategy {
 
-use Core\Access\DatabaseConnector;
+    public function requestAccess($parameters) {
+        $answer = $this->initializeAnswer();
 
-use Core\Answer\JsonAnswerParser;
 
-use Core\Access\Strategies\AbstractAccessStrategy;
+        $driver = DatabaseConnector::getDriver();
 
-class GrantViewAccessStrategy extends AbstractAccessStrategy{
-	
-	public function requestAccess($parameters){
-		$answer = $this->initializeAnswer();
-		
-		
-		$driver = DatabaseConnector::getDriver();
-		
-		$sessManager = SessionManager::getInstance();
-		$userId = $sessManager->getSessionVariable('user_id');
-		if(is_null($userId)){
-			$answer->setMessage('@user_needs_login');
-			
-		}else if($userId != 0){
-			$answer->setMessage('@forbidden');
-			
-		}else if(!isset($parameters['user']) || !isset($parameters['objects'])){
-			$answer->setMessage('@bad_parameters');
-			
-		}else if(!is_int($parameters['user']) || !is_array($parameters['objects']) ){
-			$answer->setMessage('@bad_types');
-			
-		}else{
-			$grantUserId = $parameters['user'];
-			$noError = true;
-			$queries = array();
-			foreach($parameters['objects'] as $objectId){
-				if(!is_int($objectId)){
-					$answer->setMessage('@bad_types');
-					$noError = false;
-					break;
-				}else{
-					$insertQuery = new InsertQuery();
-					$insertQuery->setEntity('uhb_user_views');
-					$insertQuery->setRowData('user_id', $grantUserId);
-					$insertQuery->setRowData('object_id', $objectId);
-					
-					$queries[] = $insertQuery;
-				}
-			}
-			
-			if($noError){
-				foreach ($queries as $query){
-					$driver->exec($query->getInstruction());
-				}
-				
-				$answer->setStatus(1);
-				$answer->setMessage('@success');
-			}
-		}
-		
-		$this->parseAnswer($answer, $parser);
-	}
-	
+        $sessManager = SessionManager::getInstance();
+        $userId = $sessManager->getSessionVariable('user_id');
+        if (is_null($userId)) {
+            $answer->setMessage('@user_needs_login');
+        } else if ($userId != 0) {
+            $answer->setMessage('@forbidden');
+        } else if (!isset($parameters['user']) || !isset($parameters['objects'])) {
+            $answer->setMessage('@bad_parameters');
+        } else if (!is_int($parameters['user']) || !is_array($parameters['objects'])) {
+            $answer->setMessage('@bad_types');
+        } else {
+            $grantUserId = $parameters['user'];
+            $noError = true;
+            $queries = array();
+            foreach ($parameters['objects'] as $objectId) {
+                if (!is_int($objectId)) {
+                    $answer->setMessage('@bad_types');
+                    $noError = false;
+                    break;
+                } else {
+                    $insertQuery = new InsertQuery();
+                    $insertQuery->setEntity('uhb_user_views');
+                    $insertQuery->setRowData('user_id', $grantUserId);
+                    $insertQuery->setRowData('object_id', $objectId);
+
+                    $queries[] = $insertQuery;
+                }
+            }
+
+            if ($noError) {
+                foreach ($queries as $query) {
+                    $driver->exec($query->getInstruction());
+                }
+
+                $answer->setStatus(1);
+                $answer->setMessage('@success');
+            }
+        }
+
+        $this->parseAnswer($answer, $parser);
+    }
+
 }
 
 ?>
